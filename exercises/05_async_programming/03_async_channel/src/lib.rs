7c899a3cb83dd540a4279fc6eac2c2539fdb0bd7
Selkous <ling_official@outlook.com>
2026-04-27 22:23:08 +0800

    05_03 finished

--- exercises/05_async_programming/03_async_channel/src/lib.rs
+++ exercises/05_async_programming/03_async_channel/src/lib.rs
@@ -18,8 +18,30 @@ pub async fn producer_consumer(items: Vec<String>) -> Vec<String> {
     // TODO: Create channel with mpsc::channel
     // TODO: Spawn producer task: iterate through items, send each one
     // TODO: Spawn consumer task: loop recv until channel closes, collect results
-    // TODO: Wait for consumer to complete and return results
-    todo!()
+    // TODO: Wait for consumer to complete and return results\
+    let (tx, mut rx) = mpsc::channel(items.len().max(1));
+
+    // producer
+    tokio::spawn(async move {
+        for i in items {
+            if tx.send(i).await.is_err() {
+                // 如果接收端关闭则退出
+                break;
+            }
+        }
+    });
+
+    let consumer = tokio::spawn(async move {
+        let mut results = Vec::new();
+
+        while let Some(item) = rx.recv().await {
+            results.push(item);
+        }
+
+        results
+    });
+
+    consumer.await.unwrap()
 }
 
 /// Fan‑in pattern: multiple producers, one consumer.
@@ -31,7 +53,28 @@ pub async fn fan_in(n_producers: usize) -> Vec<String> {
     //       Each sends format!("producer {id}: message")
     // TODO: Drop the original sender (important! otherwise channel won't close)
     // TODO: Consumer loops receiving, collects and sorts
-    todo!()
+    let (tx, mut rx) = mpsc::channel(n_producers.max(1));
+
+    for i in 0..n_producers {
+        let tx_clone = tx.clone();
+        tokio::spawn(async move {
+            tx_clone.send(format!("producer {i}: message")).await
+        }); // tx_clone will drop in here
+    }
+    drop(tx);
+
+    let consumer = tokio::spawn(async move {
+        let mut results = Vec::new();
+
+        while let Some(item) = rx.recv().await {
+            results.push(item);
+        }
+
+        results.sort();
+        results
+    });
+
+    consumer.await.unwrap()
 }
 
 #[cfg(test)]
